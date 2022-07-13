db1fd90efc2d1574a465967d76da7acc642af6f0
modehans <monicaglezsanch@gmail.com>
2022-07-13 17:06:41 +0200

    Add 'use strict' and rename constants

--- js/main.js
+++ js/main.js
@@ -1,45 +1,57 @@
-const randomNumber = getRandom(100);
-const inputNumber = document.querySelector(".js_inputNumber");
-const button = document.querySelector(".js_btn");
-const text = document.querySelector(".js_text");
-const counter = document.querySelector(".js_cont");
-let acc =0;
+'use strict';
+//Elementos del HTML
+const inputNumber = document.querySelector('.js_inputNumber');
+const testButton = document.querySelector('.js_btnPlay');
+const clueText = document.querySelector('.js_clueText');
+const triesElement = document.querySelector('.js_tries');
 
+//FUNCIONES
 //Generador de números desde 1 a valor metido en linea 1
-function getRandom(max) {
+function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-//Visualización número aleatorio en consola
-console.log("Random number: " + randomNumber);
-
-function getNum(){
+//Recoge número de la usuaria
+function getNumUser() {
   const num = parseInt(inputNumber.value);
-  console.log("El número elegido es el : " + num);
+  console.log(`El número elegido es el :  ${num}`);
   return num;
 }
-
+//Escribir en el HTML las pistas
+function renderClueTex(message) {
+  clueText.innerHTML = message;
+}
+//Compara número usuaria con valor generado automáticamente
 function checkNum() {
-   const num = getNum();
+  const num = getNumUser();
   //compara el valor usuaria con valor random
-  if (num < 1 || num > 100) {
-    text.innerHTML = "El número debe estar entre 1 y 100";
+  if (isNaN(num)) {
+    renderClueTex('No es un número.Introduzca un número entre 1 y 100');
+  } else if (num < 1 || num > 100) {
+    renderClueTex('El número debe estar entre 1 y 100');
   } else if (num < randomNumber) {
-    text.innerHTML = "Demasiado bajo";
+    renderClueTex('Demasiado bajo');
   } else if (num > randomNumber) {
-    text.innerHTML = "Demasiado alto";
+    renderClueTex('Demasiado alto');
   } else if (num === randomNumber) {
-    text.innerHTML = "¡¡¡Has ganado campeona!!!";
+    renderClueTex('¡¡¡Has ganado campeona!!!');
   }
 }
- // Contador
- function upcounter() {
-  acc += 1;
-  counter.innerHTML = (`Número de intentos: ${acc}`);
+// Contador
+function upcounter() {
+  triesCounter += 1;
+  triesElement.innerHTML = ` ${triesCounter}`;
 }
 
-function handleClickButton() {
-   checkNum();
-   upcounter();
+function handleClickButton(ev) {
+  ev.preventDefault();
+  checkNum();
+  upcounter();
 }
+//EVENTOS
+testButton.addEventListener('click', handleClickButton);
 
-button.addEventListener("click", handleClickButton);
+//CÓDIGO QUE SE EJECUTA CUANDO CARGA LA PÁGINA
+let triesCounter = 0;
+//Genera un número aleatorio entre 1 y 100
+const randomNumber = getRandomNumber(100);
+console.log(`Random number: ${randomNumber}`);
